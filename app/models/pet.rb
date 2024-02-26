6dca3aa1be106f713cadcd9851bd624f6f994443
Topaz712 <tzam712@gmail.com>
2024-02-26 15:40:55 -0600

    ADDED: pet model

--- app/models/pet.rb
+++ app/models/pet.rb
@@ -1,3 +1,10 @@
 class Pet < ApplicationRecord
+  validates :name, presence: true
+  validates :species, presence: true
+  validates :breed, presence: true
+  validates :sex, presence: true
+  validates :birthday, presence: true
+  validates :is_vaccinated, presence: true
+
   belongs_to :owner
 end
