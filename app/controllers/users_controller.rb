86fe0e5ecc5f62d96db8f1bf6a2ac1dca35970a6
Topaz712 <tzam712@gmail.com>
2024-03-08 06:45:46 -0600

    EDIT: users controller(create) errors.full_messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,10 +3,11 @@ class UsersController < ApplicationController
 
   def create
     user = User.new(user_params)
+
     if user.save
       render json: user, status: :created
     else
-      render json: user.errors, status: :unprocessable_entity
+      render json: user.errors.full_messages, status: :unprocessable_entity
     end
   end
 
