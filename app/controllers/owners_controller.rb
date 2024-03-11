2aca13ae5a0650059c48ce8d67188a15208da67d
Topaz712 <tzam712@gmail.com>
2024-03-11 04:08:37 -0500

    ADD: owners controller

--- app/controllers/owners_controller.rb
+++ app/controllers/owners_controller.rb
@@ -1,2 +1,50 @@
 class OwnersController < ApplicationController
+  before_action :set_owner, only: [:show, :update, :destroy]
+  before_action :authenticate_request, only: [:index, :show, :update, :destroy]
+
+  def index
+    owners = Owner.all
+
+    render json: owners, status: :ok
+  end
+
+  def show
+    render json: @owner, status: :ok
+  end
+
+  def create
+    owner = Owner.new(owner_params)
+
+    if owner.save
+      render json: owner, status: :created
+    else
+      render json: owner.errors.full_messages, status: :unprocessable_entity
+    end
+  end
+
+  def update
+    if @owner.update(owner_params)
+      render json: @owner, status: :ok
+    else
+      render json: @owner.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @owner.destroy
+      render json: nil, status: :ok
+    else
+      render json: @owner.errors, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def set_owner
+    @owner = Owner.find(params[:id])
+  end
+
+  def owner_params
+    params.permit(:first_name, :last_name, :contact_information, :user_id)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,18 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [:show, :update, :destroy] 
   before_action :authenticate_request, except: [:create]
+
+  def index
+    users = User.all
+
+    render json: users, status: 200
+  end
+
+  def show 
+    user = User.find_by(id: params[:id])
+
+    render json: UserBlueprint.render(user, view: :normal), status: :ok
+  end
 
   def create
     user = User.new(user_params)
@@ -11,14 +24,29 @@ class UsersController < ApplicationController
     end
   end
 
-  def show 
-    user = User.find_by(id: params[:id])
 
-    render json: UserBlueprint.render(user, view: :normal), status: :ok
+  def update
+    if @user.update(user_params)
+      render json: @user, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
+  end
+
+  def destroy
+    if @user.destroy
+      render json: nil, status: :ok
+    else
+      render json: @user.errors, status: :unprocessable_entity
+    end
   end
 
   private
 
+  def set_user
+    @user = User.find(params[:id])
+  end
+  
   def user_params
     # params.require(:user)
     params.permit(:username, :email, :password, :password_confirmation)
