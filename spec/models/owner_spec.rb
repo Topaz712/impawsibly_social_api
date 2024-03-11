334063cb6ff40a7b3be6c01cff5c28ccc5be8c3f
Topaz712 <tzam712@gmail.com>
2024-03-10 23:53:14 -0500

    ADD: owner model rspec/issue with migrations

--- spec/models/owner_spec.rb
+++ spec/models/owner_spec.rb
@@ -1,5 +1,33 @@
 require 'rails_helper'
 
 RSpec.describe Owner, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  context 'Validation tests' do
+    it 'is not valid without a first name' do
+      owner = build(:owner, first_name: nil)
+      expect(owner).not_to be_valid
+    end
+
+    it 'is not valid without a last name' do
+      owner = build(:owner, last_name: nil)
+      expect(owner).not_to be_valid
+    end
+  
+    it 'is not valid without contact information' do
+      owner = build(:owner, contact_information: nil)
+      expect(owner).not_to be_valid
+    end
+
+    it 'is not valid without a user' do
+      owner = build(:owner, user: nil)
+      expect(owner).not_to be_valid
+    end
+  end
+
+  context 'Associations tests' do
+    it 'belongs to a user' do
+      owner = build(:owner)
+      expect(owner).to respond_to(:user)
+    end
+  end
+
 end
