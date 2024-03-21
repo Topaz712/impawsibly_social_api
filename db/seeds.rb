2dbdd5834417864b1a9643b746c4270b6643138a
Topaz712 <tzam712@gmail.com>
2024-03-21 10:58:48 -0500

    ADD: populate seed faker data

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,56 @@
 #   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
 #     MovieGenre.find_or_create_by!(name: genre_name)
 #   end
+
+# Users creation
+(1..5).each do |i|
+  user = User.create(
+    username: Faker::Internet.username(specifier: 3..20, separators: %w(_)),
+    email: Faker::Internet.email,
+    password: 'password',
+    password_confirmation: 'password'
+  )
+
+  # Owner profile
+  owner = user.create_owner(
+    first_name: Faker::Name.first_name,
+    last_name: Faker::Name.last_name,
+    contact_information: Faker::PhoneNumber.phone_number
+  )
+
+  # random num of pets for each owner
+  rand(1..5).times do
+    pet = owner.pets.create(
+      name: Faker::Creature::Dog.name,
+      species: 'Dog',
+      breed: Faker::Creature::Dog.breed,
+      sex: Faker::Creature::Dog.gender,
+      birthday: Faker::Date.birthday(min_age: 1, max_age: 10),
+      is_vaccinated: Faker::Boolean.boolean(true_ratio: 0.8),
+      is_fixed: Faker::Boolean.boolean(true_ratio: 0.2)
+    )
+
+    # pet profile creation
+    pet.create.profile(
+      bio: Faker::Lorem.sentence
+    )
+  end
+
+  # pet posts
+  rand(1..10).times do
+    pet.posts.create(
+      content: Faker::Lorem.paragraph
+    )
+  end
+
+  # create random playdate events
+  rand(1..3).times do
+    owner.playdates.create(
+      title: Faker::Lorem.sentence,
+      content: Faker::Lorem.paragraph,
+      pet_limit: rand(1..7),
+      start_date_time: Faker::Time.forward(days: 25, period: :morning),
+      end_date_time: Faker::Time.forward(days: 25, period: :morning)
+    )
+  end
+end
