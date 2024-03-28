62ba9d597495d331117aa425a146d04edb41ce60
Topaz712 <tzam712@gmail.com>
2024-03-28 01:26:01 -0500

    ADD: Comment blueprint view/association

--- app/blueprints/comment_blueprint.rb
+++ app/blueprints/comment_blueprint.rb
@@ -1,4 +1,10 @@
 # frozen_string_literal: true
 
 class CommentBlueprint < Blueprinter::Base
+  identifier :id
+
+  view :profile do
+    fields :content
+    association :pet, blueprint: PetBlueprint, view: :short
+  end
 end
