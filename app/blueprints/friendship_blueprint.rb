0dc3d6d583462a4f5b69d6f4f9d7124e1f21a15d
Topaz712 <tzam712@gmail.com>
2024-03-28 01:47:29 -0500

    ADD: friendship/request views/associations

--- app/blueprints/friendship_blueprint.rb
+++ app/blueprints/friendship_blueprint.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class FriendshipBlueprint < Blueprinter::Base
+  identifier :id
+
+  view :short do
+    fields :status
+    association :pet, blueprint: PetBlueprint, view: :short
+    association :friend, blueprint: PetBlueprint, view: :short
+  end
 end
--- app/blueprints/friendship_request_blueprint.rb
+++ app/blueprints/friendship_request_blueprint.rb
@@ -1,4 +1,12 @@
 # frozen_string_literal: true
 
 class FriendshipRequestBlueprint < Blueprinter::Base
+  identifier :id
+
+  view :short do
+    fields :status
+    association :sender, blueprint: PetBlueprint, view: :short
+    association :recipient, blueprint: PetBlueprint, view: :short
+    association :friendship, blueprint: FriendshipBlueprint, view: :short
+  end
 end
