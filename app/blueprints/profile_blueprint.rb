5dc4957c2571c2f1e64ec44b53a7da469ab8f0cb
Topaz712 <tzam712@gmail.com>
2024-03-28 01:23:22 -0500

    ADD: Profile blueprint views/associations

--- app/blueprints/profile_blueprint.rb
+++ app/blueprints/profile_blueprint.rb
@@ -1,4 +1,11 @@
 # frozen_string_literal: true
 
 class ProfileBlueprint < Blueprinter::Base
+  identifier :id
+  fields :bio
+
+  view :normal do
+    association :owner, blueprint: OwnerBlueprint, view: :profile
+    association :pet, blueprint: PetBlueprint, view: :profile
+  end
 end
