4f954ad106c9c066b6e4454dff2855a81bb8fda1
Topaz712 <tzam712@gmail.com>
2024-03-28 00:37:20 -0500

    ADD: Owner blueprint views/associations

--- app/blueprints/owner_blueprint.rb
+++ app/blueprints/owner_blueprint.rb
@@ -1,4 +1,22 @@
 # frozen_string_literal: true
 
 class OwnerBlueprint < Blueprinter::Base
+  identifier :id
+
+  view :normal do
+    fields :first_name, :last_name
+  end
+
+  view :long do
+    include_view :normal
+    field :contact_information
+    association :profile, blueprint: ProfileBlueprint, view: :normal
+    association :pets, blueprint: PetBlueprint, view: [:short, :long]
+    association :location, blueprint: LocationBlueprint
+  end
+
+  view :profile do
+    include_view :normal
+    include_view :long
+  end
 end
