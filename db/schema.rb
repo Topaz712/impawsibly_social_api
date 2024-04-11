8013222851c67b58a7058fe3c7af01b3d7df9f63
Topaz712 <tzam712@gmail.com>
2024-04-10 23:58:24 -0500

    ADD: like model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_04_192657) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_11_040123) do
   create_table "active_storage_attachments", force: :cascade do |t|
     t.string "name", null: false
     t.string "record_type", null: false
@@ -66,6 +66,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_192657) do
     t.integer "user_id", null: false
     t.index ["pet_id"], name: "index_friendships_on_pet_id"
     t.index ["user_id"], name: "index_friendships_on_user_id"
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "post_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["post_id"], name: "index_likes_on_post_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "locations", force: :cascade do |t|
@@ -146,6 +155,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_04_192657) do
   add_foreign_key "comments", "pets"
   add_foreign_key "friendships", "pets"
   add_foreign_key "friendships", "users"
+  add_foreign_key "likes", "posts"
+  add_foreign_key "likes", "users"
   add_foreign_key "pets", "users"
   add_foreign_key "playdate_participants", "pets"
   add_foreign_key "playdate_participants", "playdates"
