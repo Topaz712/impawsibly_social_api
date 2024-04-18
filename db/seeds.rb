d0c0d01e81b3c7eb20f3aacd11f1305ebe7fe3d4
Topaz712 <tzam712@gmail.com>
2024-04-18 15:44:15 -0500

    EDIT: changed/added faker seed file data

--- db/seeds.rb
+++ db/seeds.rb
@@ -63,23 +63,88 @@
 #   end
 # end
 
+# require 'faker'
+
+# ActiveRecord::Base.transaction do 
+#   (1..5).each do |i|
+#     user = User.create!(
+#       first_name: Faker::Name.first_name,
+#       last_name: Faker::Name.last_name,
+#       username: Faker::Internet.username(specifier: 3..20, separators: %w(_)),
+#       email: Faker::Internet.email,
+#       password: 'password',
+#       password_confirmation: 'password'
+#     )
+
+#     rand(1..5).times do
+#       pet = Pet.create!(
+#         name: Faker::Creature::Dog.name,
+#         bio: Faker::Lorem.sentence,
+#         species: 'Dog',
+#         breed: Faker::Creature::Dog.breed,
+#         sex: Faker::Creature::Dog.gender,
+#         birthday: Faker::Date.birthday(min_age: 1, max_age: 10),
+#         is_vaccinated: Faker::Boolean.boolean(true_ratio: 1),
+#         is_fixed: Faker::Boolean.boolean(true_ratio: 1),
+#         user_id: user.id
+#       )
+
+#       rand(1..10).times do
+#         #  puts pet.post
+#         Post.create!(
+#           content: Faker::Lorem.paragraph,
+#           pet_id: pet.id,
+#           user_id: user.id
+#         )
+#       end
+#     end
+
+#     rand(1..3).times do 
+#       pet = user.pets.sample
+#       puts pet
+#       # user.created_playdates.create(
+#       playdate = Playdate.create!(
+#         title: Faker::Lorem.sentence,
+#         content: Faker::Lorem.paragraph,
+#         pet_limit: rand(1..7),
+#         species_specific: Faker::Boolean.boolean(true_ratio: 1),
+#         start_date_time: Faker::Time.forward(days: 25, period: :morning),
+#         end_date_time: Faker::Time.forward(days: 25, period: :evening),
+#         user_id: user.id,
+#         pet_id: pet.id
+#       )
+
+#       user.pets.each do |pet|
+#         PlaydateParticipant.create!(
+#           user_id: user.id,
+#           pet_id: pet.id,
+#           playdate_id: playdate.id,
+#           rating: rand(1..5)
+#         )
+#       end
+#     end
+#   end
+# end
+
 require 'faker'
 
-ActiveRecord::Base.transaction do 
+ActiveRecord::Base.transaction do
+
   (1..5).each do |i|
-    user = User.create!(
+    user = User.create(
       first_name: Faker::Name.first_name,
       last_name: Faker::Name.last_name,
-      username: Faker::Internet.username(specifier: 3..20, separators: %w(_)),
-      email: Faker::Internet.email,
+      username: Faker::Internet.unique.username(specifier: 3..20, separators: %w(_)),
+      email: Faker::Internet.unique.email(domain: 'gmail.com', separators: %w(-)),
       password: 'password',
       password_confirmation: 'password'
     )
 
-    rand(1..5).times do
+    # create pets for the user
+    rand(1..3).times do
       pet = Pet.create!(
         name: Faker::Creature::Dog.name,
-        bio: Faker::Lorem.sentence,
+        bio: Faker::Book.title,
         species: 'Dog',
         breed: Faker::Creature::Dog.breed,
         sex: Faker::Creature::Dog.gender,
@@ -89,38 +154,54 @@ ActiveRecord::Base.transaction do
         user_id: user.id
       )
 
+       # create a post for the pet
       rand(1..10).times do
-        #  puts pet.post
-        Post.create!(
-          content: Faker::Lorem.paragraph,
-          pet_id: pet.id
+       post = Post.create!(
+        # content: Faker::Games::Minecraft.achievement,
+        content: Faker::Games::Overwatch.quote,
+        pet_id: pet.id,
+        user_id: user.id
+      )
+      end
+
+      # create random number of likes for the post
+      rand(1..5).times do
+        Like.create!(
+          # randomly select a user for the like
+          user_id: User.pluck(:id).sample, 
+          post_id: post.id
         )
       end
-    end
+      
+      # create comments for the post
+      rand(2..4).times do
+        Comment.create!(
+          content: Faker::JapaneseMedia::StudioGhibli.quote,
+          # Faker::Fantasy::Tolkien.poem
+          user_id: User.pluck(:id).sample, # select a user for the comment
+          commentable: post
+        )
+      end
 
-    rand(1..3).times do 
-      pet = user.pets.sample
-      puts pet
-      # user.created_playdates.create(
+      # create a playdate for the pet
       playdate = Playdate.create!(
-        title: Faker::Lorem.sentence,
-        content: Faker::Lorem.paragraph,
+        title: Faker::Games::Overwatch.quote,
+        content: Faker::Games::SuperMario.location,
         pet_limit: rand(1..7),
         species_specific: Faker::Boolean.boolean(true_ratio: 1),
         start_date_time: Faker::Time.forward(days: 25, period: :morning),
         end_date_time: Faker::Time.forward(days: 25, period: :evening),
-        pet_id: pet.id,
-        user_id: user.id
+        user_id: user.id,
+        pet_id: pet.id
       )
 
-      user.pets.each do |pet|
-        PlaydateParticipant.create!(
-          user_id: user.id,
-          pet_id: pet.id,
-          playdate_id: playdate.id,
-          rating: rand(1..5)
-        )
-      end
+      # add the user and pet as participants of the playdate
+      PlaydateParticipant.create!(
+        user_id: user.id,
+        pet_id: pet.id,
+        playdate_id: playdate.id,
+        rating: rand(1..5)
+      )
     end
   end
 end
